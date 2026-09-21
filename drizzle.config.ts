c114afb4e84fc76d382092d8a7541a828811df3d
MedElMostapha <mohamedlemineelmostapha@gmail.com>
2026-09-21 14:08:04 +0000

    some new fix

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./db/schema.ts",
   out: "./drizzle",
-  dialect: "sqlite",
+  dialect: "turso",
   dbCredentials: {
     url:
       process.env.TURSO_DATABASE_URL ??
       process.env.DATABASE_URL ??
       "file:./local.db",
+    authToken:
+      process.env.TURSO_AUTH_TOKEN ?? process.env.DATABASE_AUTH_TOKEN,
   },
 });
